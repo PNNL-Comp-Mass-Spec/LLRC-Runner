7c640172ce7e01ec328a649296d1897bcbff1d39
alchemistmatt <matthew.monroe@pnnl.gov>
2023-01-30 14:52:22 -0800

    Update batch file and example input data file

--- Docs/RunR.bat
+++ Docs/RunR.bat
@@ -1,1 +1,1 @@
-"C:\Program Files\R\R-2.15.3\bin\R.exe" CMD BATCH --vanilla --slave "C:\DMS_Programs\LLRCRunner\QCDMscript.r"
+"C:\Program Files\R\R-4.1.1\bin\x64\R.exe" CMD BATCH --vanilla --slave "C:\DMS_Programs\LLRCRunner\QCDMscript.r"
